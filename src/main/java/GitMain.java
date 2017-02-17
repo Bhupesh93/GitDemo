4f7c9bc9c9eca91de759b31ea9f73d826d759276
Boobesh S <boobesh.s@hpe.com>
2017-02-17 16:23:37 +0530

    First commit

---
+++ src/main/java/GitMain.java
@@ -0,0 +1,16 @@
+/**
+ * Created by Boobesh S on 2/17/2017.
+ */
+public class GitMain {
+
+    public static void main(String arg[]) {
+        System.out.println("This is git demo ");
+        print("git", 100);
+
+    }
+
+    private static void print(String word, int noOfTimes) {
+        for (int i = 0; i < noOfTimes; i++)
+            System.out.println(word);
+    }
+}
